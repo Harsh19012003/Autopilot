7a27ed5f02e36362c032f3960832b4b8d0454140
gauthamkuckian <kuckiangautham@gmail.com>
2023-02-26 23:02:50 +0530

    everything working in one env

---
+++ detect.py
@@ -0,0 +1,299 @@
+import argparse
+import time
+from pathlib import Path
+import numpy as np
+import os
+from scipy import optimize
+from matplotlib import pyplot as plt
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+import glob
+import utils
+from imutils.video import VideoStream
+from midas.model_loader import default_models, load_model
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+
+from lane_detection import processImage, perspectiveWarp, plotHistogram, slide_window_search, general_search, draw_lane_lines
+from top_view_visualization import top_view
+from run import run
+
+def detect(save_img=False):
+    source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Directories
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize yolov7
+    set_logging()
+    device = select_device(opt.device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model yolov7
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+    print("Yolov7 model loaded")
+    # Load model midas
+    model_midas, transform, net_w, net_h = load_model(device, opt.model_weights, opt.model_type, optimize, opt.height, opt.square)
+    print("MiDaS model loaded")
+
+    if trace:
+        model = TracedModel(model, device, opt.img_size)
+
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    # Set Dataloader
+    # FOR TESTING INPUT VIA DROIDCAM PORT
+    '''vid = cv2.VideoCapture(2)
+    while(True):
+        ret, frame = vid.read()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break'''
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+        print("LLLLLLLLLLLLLLLLLLLLLLLLl")
+        print(type(dataset))
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        t1 = time_synchronized()
+        pred = model(img, augment=opt.augment)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        t2 = time_synchronized()
+
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        midas_img = run(im0s[0], model_midas, transform, net_w, net_h, device, opt.input_path, opt.output_path, opt.model_type, opt.optimize, opt.side, opt.height, opt.square, opt.grayscale)
+
+        detection_base_points = []
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    if save_img or view_img:  # Add bbox to image
+                        label = f'{names[int(cls)]} {conf:.2f}'
+                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2)
+                        plot_one_box(xyxy, midas_img, label=label, color=None, line_thickness=1)
+
+                    detection_base_points.append((int((int(xyxy[0])+int(xyxy[2]))/2), int(xyxy[3])))
+                # print(f"detection_base_points: {detection_base_points}")
+
+            # Print time (inference + NMS)
+            print(f'YOLOv4 FPS: {t2 - t1:.3f}')
+
+            # Stream results
+            if view_img:
+                cv2.imshow(str(p), im0)
+                cv2.waitKey(1)
+                cv2.imshow("midas", midas_img)
+                cv2.waitKey(1)  # 1 millisecond
+
+            # Midas run
+            # run(im0, model_midas, transform, net_w, net_h, device, opt.input_path, opt.output_path, opt.model_type, opt.optimize, opt.side, opt.height, opt.square, opt.grayscale)
+
+            top_view(detection_base_points)
+
+            '''# Lane detection cv2
+            frame = cv2.resize(im0, (1280, 720))
+
+            birdView, minverse = perspectiveWarp(frame)
+            # # birdView, birdViewL, birdViewR, minverse = perspectiveWarp(frame)
+
+            # 1- an already perspective warped image to process (birdView)
+            img, hls, grayscale, thresh, blur, canny = processImage(birdView)
+            hist= plotHistogram(thresh)
+            ploty, left_fit, right_fit, left_fitx, right_fitx = slide_window_search(thresh, hist)
+            draw_info = general_search(thresh, left_fit, right_fit)
+            # Filling the area of detected lanes with green
+            result = draw_lane_lines(frame, thresh, minverse, draw_info)
+            # print(type(result))
+            
+            # cv2.imshow("Final", result) #finalImg'''
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                    print(f" The image with the result is saved in: {save_path}")
+                else:  # 'video' or 'stream'
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+                        if vid_cap:  # video
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        else:  # stream
+                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            save_path += '.mp4'
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer.write(im0)
+
+    if save_txt or save_img:
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        #print(f"Results saved to {save_dir}{s}")
+
+    print(f'Done. ({time.time() - t0:.3f}s)')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+
+    parser.add_argument('-i', '--input_path',
+                        default=None,
+                        help='Folder with input images (if no input path is specified, images are tried to be grabbed '
+                             'from camera)'
+                        )
+
+    parser.add_argument('-o', '--output_path',
+                        default=None,
+                        help='Folder for output images'
+                        )
+
+    parser.add_argument('-m', '--model_weights',
+                        default="weights/dpt_swin2_tiny_256.pt",
+                        help='Path to the trained weights of model'
+                        )
+
+    parser.add_argument('-t', '--model_type',
+                        default='dpt_beit_large_512',
+                        help='Model type: '
+                             'dpt_beit_large_512, dpt_beit_large_384, dpt_beit_base_384, dpt_swin2_large_384, '
+                             'dpt_swin2_base_384, dpt_swin2_tiny_256, dpt_swin_large_384, dpt_next_vit_large_384, '
+                             'dpt_levit_224, dpt_large_384, dpt_hybrid_384, midas_v21_384, midas_v21_small_256 or '
+                             'openvino_midas_v21_small_256'
+                        )
+
+    parser.add_argument('-s', '--side',
+                        action='store_true',
+                        help='Output images contain RGB and depth images side by side'
+                        )
+
+    parser.add_argument('--optimize', dest='optimize', action='store_true', help='Use half-float optimization')
+    parser.set_defaults(optimize=False)
+
+    parser.add_argument('--height',
+                        type=int, default=None,
+                        help='Preferred height of images feed into the encoder during inference. Note that the '
+                             'preferred height may differ from the actual height, because an alignment to multiples of '
+                             '32 takes place. Many models support only the height chosen during training, which is '
+                             'used automatically if this parameter is not set.'
+                        )
+    parser.add_argument('--square',
+                        action='store_true',
+                        help='Option to resize images to a square resolution by changing their widths when images are '
+                             'fed into the encoder during inference. If this parameter is not set, the aspect ratio of '
+                             'images is tried to be preserved if supported by the model.'
+                        )
+    parser.add_argument('--grayscale',
+                        action='store_true',
+                        help='Use a grayscale colormap instead of the inferno one. Although the inferno colormap, '
+                             'which is used by default, is better for visibility, it does not allow storing 16-bit '
+                             'depth values in PNGs but only 8-bit ones due to the precision limitation of this '
+                             'colormap.'
+                        )
+
+
+    opt = parser.parse_args()
+    print(opt)
+    #check_requirements(exclude=('pycocotools', 'thop'))
+
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov7.pt']:
+                detect()
+                strip_optimizer(opt.weights)
+        else:
+            detect()
---
+++ top_view_visualization.py
@@ -0,0 +1,70 @@
+import cv2
+import imutils
+import numpy as np
+def top_view(points):
+    # print("entered tpv")
+    # initialize image
+    blank_image = np.zeros((500,500,3), np.uint8)
+    background = blank_image
+    background_height, background_width, channels = background.shape
+    car = cv2.imread('car.jpeg')
+    car = imutils.resize(car, width=20)
+
+    '''cv2.imshow("car", car)
+    cv2.waitKey(1)'''
+
+
+    # is point pe image aana chahiye
+    #external_points_set = ((0, 0), (200, 200), (100, 100), (135, 265), (150, 200), (299, 150), (389, 473), (378, 465), (500, 500))
+    external_points_set = points
+
+    # print(f"external_points_set: {external_points_set}")
+
+    for external_point in external_points_set.copy():
+        height, width, channel = car.shape
+        # print(f"height {height}, width {width}")
+        # external point should be center but due to opencv its top left corner
+
+        x, y = external_point
+        # opencv point is shift of origin
+        opencv_point = (x-int(height/2), y-int(width/2))
+        x, y = opencv_point
+
+        # error checking for out of region points
+        if x <= 0 or y <= 0:
+            # print(f"point {external_point} is not displayed on top view beacause its out of region")
+            external_points_set.remove(external_point)
+            continue
+        # print(f"opencv points {x} and  {y}")
+
+        # I want to put logo on opencv point, So I create a ROI
+        rows,cols,channels = car.shape
+        # print(f"rows {rows}, columns {cols}")
+
+        # error checking for out of region points
+        if rows+x >= background_height or cols+y >= background_width:
+            # print(f"point {external_point} is not displayed on top view beacause its out of region")
+            external_points_set.remove(external_point)
+            continue
+
+        roi = background[x:rows+x, y:cols+y ]
+
+        # Now create a mask of logo and create its inverse mask also
+        img2gray = cv2.cvtColor(car,cv2.COLOR_BGR2GRAY)
+        ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+        mask_inv = cv2.bitwise_not(mask)
+
+        # Now black-out the area of logo in ROI
+        img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+
+        # Take only region of logo from logo image.
+        img2_fg = cv2.bitwise_and(car,car,mask = mask)
+
+
+        # Put logo in ROI and modify the main image
+        dst = cv2.add(img1_bg,img2_fg)
+        background[x:rows+x, y:cols+y ] = dst
+
+    cv2.imshow('top_view',background)
+    cv2.waitKey(1)
+    return
